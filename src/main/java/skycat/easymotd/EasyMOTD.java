1ca13f3cecc721941a942cc38dc08fb6a17ccee6
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-08-26 21:52:28 -0700

    Initial commit

---
+++ src/main/java/skycat/easymotd/EasyMOTD.java
@@ -0,0 +1,10 @@
+package skycat.easymotd;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EasyMOTD implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
